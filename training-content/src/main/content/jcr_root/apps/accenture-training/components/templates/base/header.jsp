341cbdd17f878c6c537ec19ea83a066048bd2d1b
gatencio <atencio.g25@gmail.com>
2013-12-18 14:48:59 -0600

    Navegacion header

--- training-content/src/main/content/jcr_root/apps/accenture-training/components/templates/base/header.jsp
+++ training-content/src/main/content/jcr_root/apps/accenture-training/components/templates/base/header.jsp
@@ -1,8 +1,13 @@
 <%@ include file="/libs/foundation/global.jsp" %>
 <%@page import="com.accenture.cq5.HomePage" %>
 <%
-	
+	HomePage nav = new HomePage(currentPage, currentStyle);
+	pageContext.setAttribute("nav", nav);
 %>
 <header class="header">
-    <h1>HEADER</h1>
+    <c:forEach var="page" items="${nav.childrenList}">
+    	<nav>
+    		<a href="${page.url}.html">${page.title}</a>
+    	</nav>
+    </c:forEach>
 </header>
--- training-core/src/main/java/com/accenture/cq5/HomePage.java
+++ training-core/src/main/java/com/accenture/cq5/HomePage.java
@@ -9,17 +9,55 @@ import com.day.cq.wcm.api.designer.Style;
 
 public class HomePage {
 
-	private List<Page> childrenList; 
+	private List<NavigationPage> childrenList; 
+	private String imagePath;
 	
-	public HomePage(Page pPage, Style pCurrentStyle)
+	public HomePage(Page pCurrentPage, Style pCurrentStyle)
 	{
-		Iterator<Page> iterator = pPage.listChildren();
+		childrenList = generateNavPages(pCurrentPage);
+		
+		if(pCurrentStyle != null){
+			setImagePath(pCurrentStyle.get("imagePath","path")); 
+		}
 	}
 	
-	public List<Page> getChildrenList() {
+	/**
+	 * Genera la lista de paginas que apareceran en el menu de navegación
+	 * @param pCurrentPage Pagina padre de todas las paginas navegables
+	 * @return Lista de paginas. En caso de que el padre sea null la lista estara vacía
+	 */
+	private List<NavigationPage> generateNavPages(Page pCurrentPage){
+		
+		List<NavigationPage> result = new ArrayList<NavigationPage>();
+		
+		if(pCurrentPage != null){
+			Iterator<Page> children = pCurrentPage.listChildren();
+			
+			while (children.hasNext()) {
+                Page child = children.next();
+                if (!pCurrentPage.equals(child)) {
+                        NavigationPage np = new NavigationPage(child);
+                        result.add(np);
+                }
+			}
+		}
+		
+		return result;
+	}
+	
+	public List<NavigationPage> getChildrenList() {
 		return childrenList;
 	}
-	public void setChildrenList(List<Page> childrenList) {
+	
+	public void setChildrenList(List<NavigationPage> childrenList) {
 		this.childrenList = childrenList;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
 	}
 }
